1fdf20e9bd3f59b0b3f066ac87e355ee34ad29d3
junseo kim <junseokim0865@gmail.com>
2022-10-28 12:28:48 -0400

    mail-sending 1st try

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { Provider } from 'react-redux'
-import { ToastProvider } from 'react-toast-notifications';
-import initStore from './store'
+import { Provider } from "react-redux";
+import { ToastProvider } from "react-toast-notifications";
+import initStore from "./store";
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
-import ServiceApp from 'ServiceApp';
+import ServiceApp from "ServiceApp";
 
-import { onAuthStateChange, storeAuthUser } from 'actions';
+import { onAuthStateChange, storeAuthUser, sendMail } from "actions";
 
 // React Component
 // functional component
-const store = initStore()
+const store = initStore();
 
 class App extends React.Component {
-
   componentDidMount() {
     // this.unsubscribeAuth = onAuthStateChange(authUser => {
     //   store.dispatch(storeAuthUser(authUser))
 
     // })
-    onAuthStateChange(authUser => {
-      store.dispatch(storeAuthUser(authUser))
+    onAuthStateChange((authUser) => {
+      store.dispatch(storeAuthUser(authUser));
 
       // if (authUser) {
       //   store.dispatch(subscribeToMessages(authUser.uid))}
-      })
+    });
+    sendMail();
   }
 
   // componentWillMount() {
@@ -46,7 +46,4 @@ class App extends React.Component {
   }
 }
 
-
-
 export default App;
-

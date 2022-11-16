cf22e03cbd67534b5a45a2863ee217c1451ac1df
suhdanny <suhdong21@gmail.com>
2022-11-16 15:17:27 -0500

    Store daily check-in results to firestore and update check-in status

--- src/pages/CheckIn.js
+++ src/pages/CheckIn.js
@@ -1,40 +1,51 @@
 import React, { useState, useCallback } from "react";
+import { collection, doc, addDoc, updateDoc } from "firebase/firestore";
 import { Navigate } from "react-router-dom";
 import "survey-core/defaultV2.min.css";
 import { StylesManager, Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import surveyJSON from "data/survey";
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
 import { useStore } from "react-redux";
-const db = firebase.firestore();
+import db from "../db/index";
 
 StylesManager.applyTheme("defaultV2");
 
 const CheckIn = () => {
-  const store = useStore();
-  const uid = store.getState().auth.user?.uid;
-  const [redirect, setRedirect] = useState(false);
-  const survey = new Model(surveyJSON);
+	const store = useStore();
+	const uid = store.getState().auth.user?.uid;
+	const [redirect, setRedirect] = useState(false);
+	const survey = new Model(surveyJSON);
 
-  const alertResults = useCallback((sender) => {
-    const results = JSON.stringify(sender.data);
-    alert(results); // alert result in JSON format
-    setRedirect(true); // redirect to main page
-  }, []);
+	const addCheckInResult = useCallback(
+		async sender => {
+			const results = sender.data;
+			await addDoc(collection(db, "profiles", uid, "checkIns"), {
+				...results,
+				date: new Date().toLocaleDateString(),
+			});
+			setRedirect(true);
+		},
+		[uid]
+	);
 
-  survey.onComplete.add(alertResults);
+	const updateCheckInStatus = async () => {
+		await updateDoc(doc(db, "profiles", uid), {
+			checkin: true,
+		});
+	};
 
-  if (redirect) {
-    db.collection("profiles").doc(uid).update({ checkin: true });
-    return <Navigate to="/" />;
-  }
+	survey.onComplete.add(addCheckInResult);
 
-  return (
-    <div id="surveyElement">
-      <Survey model={survey} />
-    </div>
-  );
+	if (redirect) {
+		updateCheckInStatus();
+		return <Navigate to='/' />;
+	}
+
+	return (
+		<div id='surveyElement'>
+			<Survey model={survey} />
+		</div>
+	);
 };
 
 export default CheckIn;

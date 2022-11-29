112d5460bd9e9632f949ee477926b19c332ce1c1
suhdanny <suhdong21@gmail.com>
2022-11-29 12:39:04 -0500

    Add firebase cloud function for updating quarterlyCheckin field from true to false every 3 months

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -4,13 +4,19 @@ module.exports = {
     es6: true,
     node: true,
   },
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module",
+  },
   extends: ["eslint:recommended", "google"],
   rules: {
     "quotes": ["error", "double"],
+    "arrow-parens": ["error", "as-needed"],
     "linebreak-style": 0,
     "indent": 0,
     "object-curly-spacing": 0,
     "max-len": ["error", { "code": 1000 }],
     "space-before-function-paren": ["error", "always"],
+    "require-jsdoc": 0,
   },
 };

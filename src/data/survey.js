34a54204c3df165fee1e6d0b2daba909414f9eea
suhdanny <suhdong21@gmail.com>
2022-11-28 21:44:27 -0500

    Add awareness item

--- src/data/survey.js
+++ src/data/survey.js
@@ -30,7 +30,6 @@ const surveyJSON = {
     // 	],
     // },
     {
-      navigationTitle: "Daily Mood",
       elements: [
         {
           type: "imagepicker",
@@ -83,69 +82,15 @@ const surveyJSON = {
           rateMax: 10,
           isRequired: true,
         },
-      ],
-    },
-    {
-      navigationTitle: "Thoughts and Experiences",
-      elements: [
         {
-          name: "thoughts",
-          type: "matrix",
-          title:
-            "Below you will find a list of statements concerned with your thoughts and experiences. Please use the scale below to make your choice.",
-          columns: [
-            {
-              value: 5,
-              text: "Strongly agree",
-            },
-            {
-              value: 4,
-              text: "Agree",
-            },
-            {
-              value: 3,
-              text: "Neutral",
-            },
-            {
-              value: 2,
-              text: "Disagree",
-            },
-            {
-              value: 1,
-              text: "Strongly disagree",
-            },
-          ],
-          rows: [
-            {
-              value: "DC1",
-              text: "I am able to watch myself thinking.",
-            },
-            {
-              value: "DC2",
-              text: "I can watch my thoughts and emotions drift by like leaves on a stream.",
-            },
-            {
-              value: "DC3",
-              text: "I can watch my thoughts and emotions come and go like clouds.",
-            },
-            {
-              value: "RP1",
-              text: "When I wanted to feel more positive emotion, I changed the way I was thinking about the situation.",
-            },
-            {
-              value: "RP2",
-              text: "I try to think of positive consequences from my experiences.",
-            },
-          ],
-          alternateRows: true,
-          isAllRowRequired: true,
+          name: "awareness",
+          type: "rating",
+          title: "To what extend are you aware of your thoughts and emotions?",
+          minRateDescription: "Not Aware At All",
+          maxRateDescription: "Extremely Aware",
+          rateMax: 7,
+          isRequired: true,
         },
-      ],
-      showQuestionNumbers: "off",
-    },
-    {
-      navigationTitle: "Daily Reflection",
-      elements: [
         {
           name: "reflection",
           title: "Please write your daily reflection for today.",
@@ -154,6 +99,64 @@ const surveyJSON = {
         },
       ],
     },
+    // {
+    //   navigationTitle: "Thoughts and Experiences",
+    //   elements: [
+    //     {
+    //       name: "thoughts",
+    //       type: "matrix",
+    //       title:
+    //         "Below you will find a list of statements concerned with your thoughts and experiences. Please use the scale below to make your choice.",
+    //       columns: [
+    //         {
+    //           value: 5,
+    //           text: "Strongly agree",
+    //         },
+    //         {
+    //           value: 4,
+    //           text: "Agree",
+    //         },
+    //         {
+    //           value: 3,
+    //           text: "Neutral",
+    //         },
+    //         {
+    //           value: 2,
+    //           text: "Disagree",
+    //         },
+    //         {
+    //           value: 1,
+    //           text: "Strongly disagree",
+    //         },
+    //       ],
+    //       rows: [
+    //         {
+    //           value: "DC1",
+    //           text: "I am able to watch myself thinking.",
+    //         },
+    //         {
+    //           value: "DC2",
+    //           text: "I can watch my thoughts and emotions drift by like leaves on a stream.",
+    //         },
+    //         {
+    //           value: "DC3",
+    //           text: "I can watch my thoughts and emotions come and go like clouds.",
+    //         },
+    //         {
+    //           value: "RP1",
+    //           text: "When I wanted to feel more positive emotion, I changed the way I was thinking about the situation.",
+    //         },
+    //         {
+    //           value: "RP2",
+    //           text: "I try to think of positive consequences from my experiences.",
+    //         },
+    //       ],
+    //       alternateRows: true,
+    //       isAllRowRequired: true,
+    //     },
+    //   ],
+    //   showQuestionNumbers: "off",
+    // },
   ],
   showQuestionNumbers: "false",
 };

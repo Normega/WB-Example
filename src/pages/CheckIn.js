aec45411e0ad0399a1d600188516bddeadae38c7
suhdanny <suhdong21@gmail.com>
2022-11-29 00:21:28 -0500

    Add quarterly check-in feature with conditional survey JSON rendering and push quarterly check-in data to firebase

--- src/pages/CheckIn.js
+++ src/pages/CheckIn.js
@@ -1,51 +1,87 @@
-import React, { useState, useCallback } from "react";
-import { collection, doc, addDoc, updateDoc } from "firebase/firestore";
+import React, { useState, useCallback, useEffect } from "react";
+import { collection, doc, getDoc, addDoc, updateDoc } from "firebase/firestore";
 import { Navigate } from "react-router-dom";
 import "survey-core/defaultV2.min.css";
 import { StylesManager, Model } from "survey-core";
 import { Survey } from "survey-react-ui";
-import surveyJSON from "data/survey";
+import { getSurveyJSON } from "data/survey";
 import { useStore } from "react-redux";
 import db from "../db/index";
 
 StylesManager.applyTheme("defaultV2");
 
 const CheckIn = () => {
-	const store = useStore();
-	const uid = store.getState().auth.user?.uid;
-	const [redirect, setRedirect] = useState(false);
-	const survey = new Model(surveyJSON);
+    const store = useStore();
+    const uid = store.getState().auth.user?.uid;
+    const [redirect, setRedirect] = useState(false);
+    const [isQuarterlyCheckin, setIsQuarterlyCheckin] = useState(false);
+    const [surveyJSON, setSurveyJSON] = useState({});
 
-	const addCheckInResult = useCallback(
-		async sender => {
-			const results = sender.data;
-			await addDoc(collection(db, "profiles", uid, "checkIns"), {
-				...results,
-				date: new Date().toLocaleDateString(),
-			});
-			setRedirect(true);
-		},
-		[uid]
-	);
+    const survey = new Model(surveyJSON);
 
-	const updateCheckInStatus = async () => {
-		await updateDoc(doc(db, "profiles", uid), {
-			checkin: true,
-		});
-	};
+    const addCheckInResult = useCallback(
+        async sender => {
+            const results = sender.data;
 
-	survey.onComplete.add(addCheckInResult);
+            // store quarterly check-in data to quarterlyCheckIns sub-collection
+            if (isQuarterlyCheckin) {
+                const quarterlySurveyJSON = (({ wellbeingDefinition, wellbeingDomainRanking }) => ({
+                    wellbeingDefinition,
+                    wellbeingDomainRanking,
+                    date: new Date().toLocaleDateString(),
+                }))(results);
 
-	if (redirect) {
-		updateCheckInStatus();
-		return <Navigate to='/' />;
-	}
+                await addDoc(
+                    collection(db, "profiles", uid, "quarterlyCheckIns"),
+                    quarterlySurveyJSON
+                );
+            }
 
-	return (
-		<div id='surveyElement'>
-			<Survey model={survey} />
-		</div>
-	);
+            // store daily check-in data to checkIns sub-collection
+            await addDoc(collection(db, "profiles", uid, "checkIns"), {
+                ...results,
+                date: new Date().toLocaleDateString(),
+            });
+
+            setRedirect(true);
+        },
+        [uid, isQuarterlyCheckin]
+    );
+
+    const updateCheckInStatus = async () => {
+        if (isQuarterlyCheckin) {
+            await updateDoc(doc(db, "profiles", uid), {
+                quarterlyCheckin: true,
+            });
+        }
+        await updateDoc(doc(db, "profiles", uid), {
+            checkin: true,
+        });
+    };
+
+    useEffect(() => {
+        const loadSurveyJSON = async () => {
+            const docRef = doc(db, "profiles", uid);
+            const docSnap = await getDoc(docRef);
+            const quarterlyCheckin = docSnap.data().quarterlyCheckin;
+            setIsQuarterlyCheckin(!quarterlyCheckin);
+            setSurveyJSON(getSurveyJSON(quarterlyCheckin));
+        };
+        loadSurveyJSON();
+    }, [uid, isQuarterlyCheckin]);
+
+    survey.onComplete.add(addCheckInResult);
+
+    if (redirect) {
+        updateCheckInStatus();
+        return <Navigate to='/' />;
+    }
+
+    return (
+        <div id='surveyElement'>
+            <Survey model={survey} />
+        </div>
+    );
 };
 
 export default CheckIn;

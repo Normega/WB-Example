c3ed8b12e24be8f3aef02b0d60a3de9cdb43fa61
junseo kim <junseokim0865@gmail.com>
2023-01-27 13:32:37 -0500

    mailing function done

--- functions/actions/mailHelper.js
+++ functions/actions/mailHelper.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
-// const admin = require("firebase-admin");
+const fetch = require("node-fetch");
+
+const admin = require("firebase-admin");
 const cors = require("cors")({
     origin: true,
     allowedHeaders: "Content-Type, Authorization",
@@ -18,7 +20,7 @@ function prepareMail (req, res) {
         auth: {
             user: "firebase@radlab.zone",
             pass: "MFtriangle",
-        },
+        }
     });
     cors(req, res, () => {
         const mailOptions = {
@@ -48,26 +50,47 @@ async function requestMail (email) {
         // params: JSON.stringify({ dest: "radlab.noreply@gmail.com" }),
     };
     //    change the POST link below
-    await fetch("http://localhost:5000/wellbeing-49fed/us-central1/sendMail", requestOptions)
+    await fetch("http://localhost:5001/wellbeing-49fed/us-central1/sendMail", requestOptions)
         .then(res => {
-            console.log(res);
+            console.log(res.text());
             // console.log(res.text())
+            // return "this works!";
             return res.text();
         })
         .then(data => {
             console.log(data);
             // console.log(data.json());
         });
+    // return await response.text();
 }
 exports.requestMail = requestMail;
 
 exports.sendMail = functions.https.onRequest((req, res) => {
     prepareMail(req, res);
 });
+// Iterates thrur database and collects emails
+function collectEmails () {
+    const data = [];
+    admin
+    .firestore()
+    .collection("profiles")
+    .get()
+    .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            data.push(doc.data());
+        });
+    });
+    return data;
+}
+// testing
+exports.collectEmails = collectEmails;
 
-// exports.requestMail = functions.pubsub
-//     .schedule("every day 06:00")
-//     .timeZone("America/Toronto")
-//     .onRun(context => {
-//         requestMail("radlab.noreply@gmail.com");
-//     });
+exports.weeklySendMail = functions.pubsub
+    .schedule("every day 06:00")
+    .timeZone("America/Toronto")
+    .onRun(context => {
+        // return requestMail("radlab.noreply@gmail.com");
+        collectEmails().forEach(doc => {
+            requestMail(doc.email);
+        });
+    });

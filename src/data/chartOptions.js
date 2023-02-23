10095db155440e06bcbd31f62d2dba2778308a12
suhdanny <suhdong21@gmail.com>
2023-02-23 16:41:11 -0500

    Add two graphs: mood and stress line chart and awareness, reactivity, and appraisal line chart

---
+++ src/data/chartOptions.js
@@ -0,0 +1,33 @@
+const moodStressOption = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "bottom",
+        },
+        title: {
+            display: true,
+            text: "Your Happiness and Stress Level",
+            font: {
+                size: 15,
+            },
+        },
+    },
+};
+
+const questionOption = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "bottom",
+        },
+        title: {
+            display: true,
+            text: "Awareness, Reactivity, and Reappraisal",
+            font: {
+                size: 15,
+            },
+        },
+    },
+};
+
+export { moodStressOption, questionOption };

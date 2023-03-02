f2435313cb5c6bac0400d0f10d8713ac6b57983c
suhdanny <suhdong21@gmail.com>
2023-03-02 00:05:21 -0500

    Reimplement the home page according to new/suggested UI/UX design

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
-import HomePage from "./pages/Home"; //the imported name (e.g., HomePage) could be whatever you want
 import ProfilePage from "./pages/Profile";
 import ServicesPage from "./pages/Services";
 import ServiceDetailPage from "./pages/ServiceDetail";
@@ -14,24 +12,25 @@ import ServiceCreatePage from "pages/services/ServiceCreate";
 import UserServicesPage from "pages/services/UserServices";
 import CheckIn from "pages/CheckIn";
 import UserDashboard from "pages/UserDashboard";
+import Hero from "components/Hero";
 
 const Routess = () => (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    {/* service id is dynamic */}
-    <Route path="/services/me" element={<UserServicesPage />} />
-    <Route path="/services/new" element={<ServiceCreatePage />} />
-    <Route path="/services/:serviceId" element={<ServiceDetailPage />} />
-    <Route path="/services" element={<ServicesPage />} />
-    <Route path="/profile" element={<ProfilePage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<RegisterPage />} />
-    <Route path="/secret" element={<SecretPage />} />
-    <Route path="/avatar-creation" element={<AvatarCreation />} />
-    <Route path="/definition" element={<WellnessDefinition />} />
-    <Route path="/checkin" element={<CheckIn />} />
-    <Route path="/dashboard" element={<UserDashboard />} />
-  </Routes>
+    <Routes>
+        <Route path='/' element={<Hero />} />
+        {/* service id is dynamic */}
+        <Route path='/services/me' element={<UserServicesPage />} />
+        <Route path='/services/new' element={<ServiceCreatePage />} />
+        <Route path='/services/:serviceId' element={<ServiceDetailPage />} />
+        <Route path='/services' element={<ServicesPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/secret' element={<SecretPage />} />
+        <Route path='/avatar-creation' element={<AvatarCreation />} />
+        <Route path='/definition' element={<WellnessDefinition />} />
+        <Route path='/checkin' element={<CheckIn />} />
+        <Route path='/dashboard' element={<UserDashboard />} />
+    </Routes>
 );
 
 export default Routess;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,32 +1,68 @@
-/* eslint jsx-a11y/anchor-is-valid: 0 */
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/Hero.css";
 
-import React from 'react'
+const Hero = () => {
+    const navigate = useNavigate();
 
-
-const Hero = () => 
-  <section className="hero is-default is-bold">
-    <div className="hero-body">
-      <div className="container">
-        <div className="columns is-vcentered">
-          <div className="column is-5 is-offset-1 landing-caption">
-            <h1 className="title is-1 is-bold is-spaced">
-                Learn, Collaborate.
-            </h1>
-            <h2 className="subtitle is-5 is-muted">Lorem ipsum sit dolor amet is a dummy text used by typography industry </h2>
-            <p>
-              <a className="button cta rounded primary-btn raised">
-                  Get Started
-              </a>
-            </p>
-          </div>
-          <div className="column is-5 is-offset-1">
-            <figure className="image is-4by3">
-                <img src={process.env.PUBLIC_URL + '/worker.svg'} alt="Description" />
-            </figure>
-          </div>
+    return (
+        <div>
+            <section className='hero-main-container'>
+                <div className='hero-left-container'>
+                    <h1 className='left-title'>Your Customized Wellness Buddy</h1>
+                    <p className='left-subtitle'>
+                        Measure your mental health and wellness with data-driven insights and
+                        personalized feedback
+                    </p>
+                    <button className='started-button' onClick={() => navigate("/register")}>
+                        Get Started
+                    </button>
+                </div>
+                <div className='hero-right-container'>
+                    <img src={process.env.PUBLIC_URL + "/worker.svg"} alt='Description' />
+                </div>
+            </section>
+            <section className='hero-info-container'>
+                <div className='info-card first'>
+                    <img src='/images/avatar.png' alt='avatar' />
+                    <h3>Customized Avatar</h3>
+                </div>
+                <div className='info-card second'>
+                    <img src='/images/mood.png' alt='mood-timeline' />
+                    <h3>Mood Tracking</h3>
+                </div>
+                <div className='info-card third'>
+                    <img src='images/lightbulb.png' alt='lighbulb' />
+                    <h3>Individualized Feedback</h3>
+                </div>
+            </section>
         </div>
-      </div>
-    </div>
-  </section>
+    );
+};
+//     <section className='hero is-default is-bold'>
+//         <div className='hero-body'>
+//             <div className='container'>
+//                 <div className='columns is-vcentered'>
+//                     <div className='column is-5 is-offset-1 landing-caption'>
+//                         <h1 className='title is-1 is-bold is-spaced'>
+//                             Your Customized Wellness Buddy
+//                         </h1>
+//                         {/* <h2 className='subtitle is-5 is-muted'>
+//                             Lorem ipsum sit dolor amet is a dummy text used by typography industry{" "}
+//                         </h2> */}
+//                         <p>
+//                             <a className='button cta rounded primary-btn raised'>Get Started</a>
+//                         </p>
+//                     </div>
+//                     <div className='column is-5 is-offset-1'>
+//                         <figure className='image is-4by3'>
+//                             <img src={process.env.PUBLIC_URL + "/worker.svg"} alt='Description' />
+//                         </figure>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </section>
+// );
 
-export default Hero
+export default Hero;

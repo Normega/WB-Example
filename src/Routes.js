1fd87e75893ac1dbe0fc6c56cb4e916c38d7e8a8
suhdanny <suhdong21@gmail.com>
2023-05-23 22:18:51 -0400

    Delete more unsued files and make minor changes

--- src/Routes.js
+++ src/Routes.js
@@ -1,37 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ProfilePage from "./pages/Profile";
-import ServicesPage from "./pages/Services";
-import ServiceDetailPage from "./pages/ServiceDetail";
 import LoginPage from "./pages/Login";
-import RegisterPage from "./pages/Register";
 import SignUp from "pages/SignUp";
-import SecretPage from "pages/Secret";
 import { AvatarCreationPage } from "pages/AvatarCreation";
-import WellnessDefinition from "pages/WBDefinition";
-import ServiceCreatePage from "pages/services/ServiceCreate";
-import UserServicesPage from "pages/services/UserServices";
 import CheckIn from "pages/CheckIn";
 import UserDashboard from "pages/UserDashboard";
 import Hero from "components/Hero";
 
-const Routess = () => (
+const AppRoutes = () => (
     <Routes>
         <Route path='/' element={<Hero />} />
-        {/* service id is dynamic */}
-        <Route path='/services/me' element={<UserServicesPage />} />
-        <Route path='/services/new' element={<ServiceCreatePage />} />
-        <Route path='/services/:serviceId' element={<ServiceDetailPage />} />
-        <Route path='/services' element={<ServicesPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<SignUp />} />
-        <Route path='/secret' element={<SecretPage />} />
-        <Route path='/definition' element={<WellnessDefinition />} />
+        <Route path='/login' element={<LoginPage />} />
         <Route path='/checkin' element={<CheckIn />} />
+        <Route path='/profile' element={<ProfilePage />} />
         <Route path='/dashboard' element={<UserDashboard />} />
-        <Route path='/avatar' element={<AvatarCreationPage />}></Route>
+        <Route path='/avatar' element={<AvatarCreationPage />} />
     </Routes>
 );
 
-export default Routess;
+export default AppRoutes;

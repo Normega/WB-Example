8129ca090a7a41eee6c402b66259583f92d67c24
suhdanny <suhdong21@gmail.com>
2023-05-23 22:04:20 -0400

    Delete unused and unnecessary npm packages

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
 import React from "react";
-
-import { Provider } from "react-redux";
-import { ToastProvider } from "react-toast-notifications";
-import Routess from "Routes.js";
 import initStore from "./store";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import ServiceApp from "ServiceApp";
-import { AvatarCreationPage } from "pages/AvatarCreation";
-
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import { onAuthStateChange, storeAuthUser } from "actions";
 
-// React Component
-// functional component
 const store = initStore();
 
 class App extends React.Component {
   componentDidMount() {
-    // this.unsubscribeAuth = onAuthStateChange(authUser => {
-    //   store.dispatch(storeAuthUser(authUser))
-
-    // })
     onAuthStateChange((authUser) => {
       store.dispatch(storeAuthUser(authUser));
-
-      // if (authUser) {
-      //   store.dispatch(subscribeToMessages(authUser.uid))}
     });
   }
-
-  // componentWillMount() {
-  //   this.unsubscribeAuth()
-  // }
 
   render() {
     return (
       <Provider store={store}>
-        <ToastProvider>
-          <Router>
-            <ServiceApp />
-          </Router>
-        </ToastProvider>
+        <Router>
+          <ServiceApp />
+        </Router>
       </Provider>
     );
   }
